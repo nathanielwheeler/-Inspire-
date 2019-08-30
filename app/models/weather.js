47110afdaac4a110c2e150cff977181109e67699
Nathaniel Wheeler <contact@nathanielwheeler.com>
2019-08-30 13:29:00 -0600

    temperature max && min

--- app/models/weather.js
+++ app/models/weather.js
@@ -9,8 +9,29 @@ export default class Weather {
     //      check out the other data that comes back and see if there is anything you want to try
 
     this.city = data.name
-    this.kelvin = data.main.temp
-    this.fahrenheit = ((this.kelvin - 273.15) * (9 / 5)) + 32
-    this.celsius = this.kelvin - 273.15
+    this.temp = {
+      current: {
+        kelvin: data.main.temp,
+        fahrenheit: ((this.temp.current.kelvin - 273.15) * (9 / 5)) + 32,
+        celsius: this.temp.current.kelvin - 273.15
+      },
+      high: {
+        kelvin: data.main.temp_max,
+        fahrenheit: ((this.temp.high.kelvin - 273.15) * (9 / 5)) + 32,
+        celsius: this.temp.high.kelvin - 273.15
+      },
+      low: {
+        kelvin: data.main.temp_min,
+        fahrenheit: ((this.temp.low.kelvin - 273.15) * (9 / 5)) + 32,
+        celsius: this.temp.low.kelvin - 273.15
+      }
+    }
+
+  }
+
+  get Template() {
+    return `
+    
+    `
   }
 }
